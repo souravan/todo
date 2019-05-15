6a7f04cd24528aa79c2857c801a419c72a6a2849
Sourav Anand <soanand14@gmail.com>
2019-05-14 22:36:12 -0700

    updated ghc version

--- src/Settings/StaticFiles.hs
+++ src/Settings/StaticFiles.hs
@@ -5,7 +5,7 @@ module Settings.StaticFiles where
 
 import Settings     (appStaticDir, compileTimeAppSettings)
 import Yesod.Static (staticFiles)
--- import qualified Prelude as p
+-- import qualified Prelude as P
 -- This generates easy references to files in the static directory at compile time,
 -- giving you compile-time verification that referenced files exist.
 -- Warning: any files added to your static directory during run-time can't be
